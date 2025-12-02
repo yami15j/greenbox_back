889e987976daae87754800b838aeb77d52657d6b
daniu006 <dpabad@sudamericano.edu.ec>
2025-12-01 20:54:46 -0500

    main

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
     transform: true,
   }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0'); 
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
